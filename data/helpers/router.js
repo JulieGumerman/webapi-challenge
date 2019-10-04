0ca7b06fe3f8014f4dd1fc4a37abb400041a046e
Julie Gumerman <julie.gumerman@gmail.com>
2019-10-04 10:54:36 -0600

    MVP

--- data/helpers/router.js
+++ data/helpers/router.js
@@ -41,21 +41,37 @@ router.put("/:id", (req, res) => {
     const changes = req.body;
 
     projects.update( id, changes )
-        .then(data => {res.json(changes)})
+        .then(data => {
+            if (!data.id){
+                res.status(404).json({error: "Why would you try to update something that doesn't exist?!?!"})
+            } else {
+              res.json(changes);                
+            }
+
+        })
         .catch(err => res.json({ message: "ruhh-roohhh"}))
 })
 
 router.delete("/:id", (req, res) => {
     const { id } = req.params;
     projects.remove(id)
-        .then(data => res.json(data))
+        .then(data => {
+            res.json(data);
+        })
         .catch(err => {res.json({ message: "too amazing to be removed"})})
 })
 
 router.get("/:id/actions", (req, res) => {
     const { id } = req.params;
     projects.getProjectActions(id)
-        .then(data => res.json(data))
+        .then(data => {
+            if (data.length > 0) {
+                res.json(data)
+            } else {
+                res.status(404).json({ message: "non-existent"})
+            }
+        
+        })
         .catch(error => {res.json({ message: "These aren't the actions you are looking for."})})
 })
 
