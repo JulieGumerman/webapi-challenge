2c6991dcf71da2af31ac795cb54d5d66df5a237f
Julie Gumerman <julie.gumerman@gmail.com>
2019-10-04 10:26:09 -0600

    everything but the if/else and other status codes

--- data/helpers/router.js
+++ data/helpers/router.js
@@ -32,4 +32,43 @@ router.put("/:id", (req, res) => {
         .catch(err => res.json({ message: "ruhh-roohhh"}))
 })
 
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    projects.remove(id)
+        .then(data => res.json(data))
+        .catch(err => {res.json({ message: "too amazing to be removed"})})
+})
+
+router.get("/:id/actions", (req, res) => {
+    const { id } = req.params;
+    projects.getProjectActions(id)
+        .then(data => res.json(data))
+        .catch(error => {res.json({ message: "These aren't the actions you are looking for."})})
+})
+
+router.post("/:id/actions", (req, res) => {
+    const  id  = req.params.project_id;
+    const newAction = req.body;
+    actions.insert( newAction)
+        .then(data => res.json(newAction))
+        .catch(err => json.json({ message: "Sometimes you have those days. This is one of them!" }))
+})
+
+router.put("/:id/actions/:id", (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+    actions.update(id, changes)
+        .then(data => res.json(changes))
+        .catch(err => res.json({ message: "better luck next time"}))
+})
+
+router.delete("/:id/actions/:id", (req, res) => {
+    const { id } = req.params;
+    actions.remove(id)
+        .then(data => res.json(data))
+        .catch(err => res.json({ message: "you're better off not deleting this one anyway!!!"}))
+})
+
+router
+
 module.exports = router;
