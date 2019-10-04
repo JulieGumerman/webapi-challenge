d52fb45aa35c4f185ddf82facc3ade5d1ed52f53
Julie Gumerman <julie.gumerman@gmail.com>
2019-10-04 09:50:23 -0600

    can update and post

--- data/helpers/router.js
+++ data/helpers/router.js
@@ -16,4 +16,20 @@ router.get("/:id", (req, res) => {
         .catch(error => res.json({ message: "oh nos!!!"}))
 })
 
+router.post("/", (req, res) => {
+    const newProject = req.body;
+    projects.insert(newProject)
+        .then(data => res.json(newProject))
+        .catch(err => res.json({ message: "Oh dear...it didn't work"}))
+})
+
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    projects.update( id, changes )
+        .then(data => {res.json(changes)})
+        .catch(err => res.json({ message: "ruhh-roohhh"}))
+})
+
 module.exports = router;
